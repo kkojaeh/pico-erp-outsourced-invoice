49184514e9ff859a580edd44a5f31f335a24a473
Ethan <kkojaeh@gmail.com>
2019-02-15 17:54:07 +0900

    ddl 생성

--- impl/src/main/resources/outsourced-invoice/flyway/mysql/V1__initialize.sql
+++ impl/src/main/resources/outsourced-invoice/flyway/mysql/V1__initialize.sql
@@ -1,4 +1,4 @@
-create table osi_outsourced_invoice (
+create table osdi_outsourced_invoice (
 	id binary(16) not null,
 	created_by_id varchar(50),
 	created_by_name varchar(50),
@@ -8,30 +8,36 @@ create table osi_outsourced_invoice (
 	last_modified_by_id varchar(50),
 	last_modified_by_name varchar(50),
 	last_modified_date datetime,
-	outsourced_order_id binary(16),
+	receive_address_detail varchar(50),
+	receive_address_postal_code varchar(10),
+	receive_address_street varchar(50),
+	receiver_id varchar(50),
 	remark varchar(50),
 	status varchar(20),
+	supplier_id varchar(50),
 	primary key (id)
 ) engine=InnoDB;
 
-create table osi_outsourced_invoice_item (
+create table osdi_outsourced_invoice_item (
 	id binary(16) not null,
 	created_by_id varchar(50),
 	created_by_name varchar(50),
 	created_date datetime,
 	invoice_id binary(16),
 	invoice_item_id binary(16),
+	item_id binary(16),
+	item_spec_code varchar(20),
 	last_modified_by_id varchar(50),
 	last_modified_by_name varchar(50),
 	last_modified_date datetime,
-	order_item_id binary(16),
 	quantity decimal(19,2),
 	remark varchar(50),
+	unit varchar(20),
 	primary key (id)
 ) engine=InnoDB;
 
-alter table osi_outsourced_invoice
-	add constraint UK3nlcyyd4o3nqq73xilbqts3y4 unique (invoice_id);
+alter table osdi_outsourced_invoice
+	add constraint UKt0gobbk7kbtr51i3i768078tm unique (invoice_id);
 
-create index IDXsb1p4n5x2pqqhqk9tw4w1cmrp
-	on osi_outsourced_invoice_item (invoice_id);
+create index IDXgt1rchhjlq7cdxxlul6s2h9mo
+	on osdi_outsourced_invoice_item (invoice_id);
